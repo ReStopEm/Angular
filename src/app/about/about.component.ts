1b522d2c9a145c1e008d3b5d5ed98ac912a70f78
lomakin <lomakin@A33COMP12>
2019-03-25 18:58:50 +0200

    init commit

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit {
+  ownerName = 'Sasha';
+  languages = ['c#','c++','Pascal', 'Vasik','Assembler'];
+    constructor() { }
+
+ngOnInit() {
+}
+
+}
